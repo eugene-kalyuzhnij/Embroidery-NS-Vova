14c42feb28a5ba4f80f20321a67a33e66b0e7381
nsvova <volodymyr.nos@globallogic.com>
2014-04-01 19:12:07 +0300

    Add control elements to form and new methods to class Form1.

--- Source/NSEmbroidery.UI/Form1.Designer.cs
+++ Source/NSEmbroidery.UI/Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBoxCurrentImage = new System.Windows.Forms.PictureBox();
+            this.buttonChooseColor = new System.Windows.Forms.Button();
+            this.panelColors = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonCreateScheme = new System.Windows.Forms.Button();
+            this.texBoxCountOfCrissCrosses = new System.Windows.Forms.TextBox();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCurrentImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 44);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(998, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // pictureBoxCurrentImage
+            // 
+            this.pictureBoxCurrentImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxCurrentImage.Location = new System.Drawing.Point(12, 82);
+            this.pictureBoxCurrentImage.Name = "pictureBoxCurrentImage";
+            this.pictureBoxCurrentImage.Size = new System.Drawing.Size(418, 389);
+            this.pictureBoxCurrentImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCurrentImage.TabIndex = 2;
+            this.pictureBoxCurrentImage.TabStop = false;
+            // 
+            // buttonChooseColor
+            // 
+            this.buttonChooseColor.Location = new System.Drawing.Point(334, 44);
+            this.buttonChooseColor.Name = "buttonChooseColor";
+            this.buttonChooseColor.Size = new System.Drawing.Size(96, 23);
+            this.buttonChooseColor.TabIndex = 3;
+            this.buttonChooseColor.Text = "Choose color";
+            this.buttonChooseColor.UseVisualStyleBackColor = true;
+            this.buttonChooseColor.Click += new System.EventHandler(this.buttonChooseColor_Click);
+            // 
+            // panelColors
+            // 
+            this.panelColors.AutoScroll = true;
+            this.panelColors.Location = new System.Drawing.Point(451, 82);
+            this.panelColors.Name = "panelColors";
+            this.panelColors.Size = new System.Drawing.Size(50, 389);
+            this.panelColors.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(524, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(414, 389);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonCreateScheme
+            // 
+            this.buttonCreateScheme.Location = new System.Drawing.Point(524, 44);
+            this.buttonCreateScheme.Name = "buttonCreateScheme";
+            this.buttonCreateScheme.Size = new System.Drawing.Size(96, 23);
+            this.buttonCreateScheme.TabIndex = 6;
+            this.buttonCreateScheme.TabStop = false;
+            this.buttonCreateScheme.Text = "Create scheme";
+            this.buttonCreateScheme.UseVisualStyleBackColor = true;
+            this.buttonCreateScheme.Click += new System.EventHandler(this.buttonCreateScheme_Click);
+            // 
+            // texBoxCountOfCrissCrosses
+            // 
+            this.texBoxCountOfCrissCrosses.Location = new System.Drawing.Point(244, 46);
+            this.texBoxCountOfCrissCrosses.Name = "texBoxCountOfCrissCrosses";
+            this.texBoxCountOfCrissCrosses.Size = new System.Drawing.Size(75, 20);
+            this.texBoxCountOfCrissCrosses.TabIndex = 7;
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(129, 40);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(109, 26);
+            this.labelCount.TabIndex = 8;
+            this.labelCount.Text = "Count of CrissCrosses\n(horisontal)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 539);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.texBoxCountOfCrissCrosses);
+            this.Controls.Add(this.buttonCreateScheme);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelColors);
+            this.Controls.Add(this.buttonChooseColor);
+            this.Controls.Add(this.pictureBoxCurrentImage);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCurrentImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBoxCurrentImage;
+        private System.Windows.Forms.Button buttonChooseColor;
+        private System.Windows.Forms.Panel panelColors;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonCreateScheme;
+        private System.Windows.Forms.TextBox texBoxCountOfCrissCrosses;
+        private System.Windows.Forms.Label labelCount;
     }
 }
 
--- Source/NSEmbroidery.UI/Form1.cs
+++ Source/NSEmbroidery.UI/Form1.cs
@@ -7,14 +7,148 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using NSEmbroidery.Core;
 
 namespace NSEmbroidery.UI
 {
     public partial class Form1 : Form
     {
+        Bitmap CurrentImage { get; set; }
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "D:\\";
+            openFileDialog1.Filter = "All files (*.*)|*.*|jpg files (*.jpg)|*.jpg|bmp files (*bmp)|*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            CurrentImage = new Bitmap(myStream);
+                            pictureBoxCurrentImage.Image = CurrentImage;
+                            pictureBoxCurrentImage.Refresh();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void buttonChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.AllowFullOpen = false;
+            MyDialog.ShowHelp = true;
+            MyDialog.Color = Color.FromArgb(255, 255, 128, 128);
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                AddColorToPanelColor(MyDialog.Color);
+        }
+
+
+        private void AddColorToPanelColor(Color color)
+        {
+            int y;
+            int countControls = panelColors.Controls.Count;
+
+            if(countControls > 0)
+                y = panelColors.Controls[countControls - 1].Location.Y + 25;
+            else y = 10;
+
+            PictureBox boxColor = new PictureBox();
+            boxColor.Size = new Size(20, 20);
+            boxColor.Location = new Point(5, y);
+            boxColor.BackColor = color;
+            boxColor.Click += boxColor_Click;
+
+            panelColors.Controls.Add(boxColor);
+            panelColors.Refresh();
+        }
+
+
+        private void boxColor_Click(object sender, EventArgs e)
+        {
+            PictureBox boxColor = sender as PictureBox;
+            if (boxColor != null)
+            {
+                panelColors.Controls.Remove(boxColor);
+                RefreshPanelColor();
+            }
+        }
+
+
+        private void RefreshPanelColor()
+        {
+            int y = 10;
+
+            foreach (PictureBox item in panelColors.Controls)
+            {
+                item.Location = new Point(5, y);
+
+                y += 25;
+            }
+        }
+
+
+        public Color[] GetColorsFromPanel()
+        {
+            Color[] result = new Color[panelColors.Controls.Count];
+            int i = 0;
+            foreach (PictureBox item in panelColors.Controls)
+            {
+                result[i++] = item.BackColor;
+            }
+
+            return result;
+        }
+
+        private void buttonCreateScheme_Click(object sender, EventArgs e)
+        {
+            //use dll here
+            if(CurrentImage == null)
+            {
+                MessageBox.Show("Open any image first!");
+                return;
+            }
+            
+            int countOfCrissCrosses;
+            try
+            {
+                countOfCrissCrosses = Convert.ToInt32(texBoxCountOfCrissCrosses.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Sure that Count of CrissCrosses is correct");
+            }
+
+            //Settings settings = new Settings(countOfCrissCrosses, 
+            //BitmapConverter converter = new BitmapConverter(CurrentImage, 
+
+
+        }
+
     }
 }

66414fd4af18ff698fc40425005d5454842a0d86
nsvova <volodymyr.nos@globallogic.com>
2014-04-11 14:52:19 +0300

    Delete previos unit tests. Add new unit test

---
+++ Source/NSEmbroidery.UnitTests/UnitTest1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSEmbroidery.Core;
+using System.Drawing;
+using System.Collections;
+
+namespace NSEmbroidery.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1_ColorDecorator()
+        {
+            Bitmap inputImage = new Bitmap(7, 5);
+
+            #region Filling inputImage
+            for (int y = 0; y < 5; y++)
+                for (int x = 0; x < 7; x++)
+                {
+                    if ((x == 2 && y == 0) ||
+                        (x == 4 && y == 0) ||
+                        (x == 5 && y == 0) ||
+                        (x == 5 && y == 1)
+                        )
+                        inputImage.SetPixel(x, y, Color.Green);
+                    else if ((x == 0 && y == 2) ||
+                            (x == 1 && y == 2) ||
+                            (x == 2 && y == 2) ||
+                            (x == 3 && y == 2) ||
+                            (x == 2 && y == 3)
+                            )
+                        inputImage.SetPixel(x, y, Color.Blue);
+                    else
+                        inputImage.SetPixel(x, y, Color.Red);
+                }
+            #endregion
+
+
+            Bitmap expectedImage = new Bitmap(6, 4);
+
+            #region Filling expectedImage
+            for (int y = 0; y < 4; y++)
+                for (int x = 0; x < 6; x++)
+                {
+                    if ((x == 4 && y == 0) ||
+                        (x == 5 && y == 0) ||
+                        (x == 4 && y == 1) ||
+                        (x == 5 && y == 1)
+                        )
+                        expectedImage.SetPixel(x, y, Color.Green);
+                    else if ((x == 2 && y == 2) ||
+                            (x == 3 && y == 2) ||
+                            (x == 2 && y == 3) ||
+                            (x == 3 && y == 3)
+                            )
+                        expectedImage.SetPixel(x, y, Color.Blue);
+                    else
+                        expectedImage.SetPixel(x, y, Color.Red);
+                }
+            #endregion
+
+
+
+            PatternCreator creator = new PatternCreator();
+            creator.Palette = new Color[] { Color.Red, Color.Green, Color.Blue };
+
+            Bitmap actual = creator.GetEmbroidery(inputImage, 3, 2);
+
+            Assert.IsTrue(actual.Height == expectedImage.Height && actual.Width == expectedImage.Width);
+
+            for (int y = 0; y < expectedImage.Height; y++)
+                for (int x = 0; x < expectedImage.Width; x++)
+                    Assert.IsTrue(actual.GetPixel(x, y) == expectedImage.GetPixel(x, y));
+
+        }
+
+        [TestMethod]
+        public void TestMethod1_ColorGridDecorator()
+        {
+            Bitmap inputImage = new Bitmap(7, 5);
+
+            #region Filling inputImage
+            for (int y = 0; y < 5; y++)
+                for (int x = 0; x < 7; x++)
+                {
+                    if ((x == 2 && y == 0) ||
+                        (x == 4 && y == 0) ||
+                        (x == 5 && y == 0) ||
+                        (x == 5 && y == 1)
+                        )
+                        inputImage.SetPixel(x, y, Color.Green);
+                    else if ((x == 0 && y == 2) ||
+                            (x == 1 && y == 2) ||
+                            (x == 2 && y == 2) ||
+                            (x == 3 && y == 2) ||
+                            (x == 2 && y == 3)
+                            )
+                        inputImage.SetPixel(x, y, Color.Blue);
+                    else
+                        inputImage.SetPixel(x, y, Color.Red);
+                }
+            #endregion
+
+
+            Bitmap expectedImage = new Bitmap(6, 4);
+
+            #region Filling expectedImage
+            for (int y = 0; y < 4; y++)
+                for (int x = 0; x < 6; x++)
+                {
+                    if ((x == 0 && y == 0) ||
+                        (x == 0 && y == 2) ||
+                        (x == 4 && y == 2) ||
+                        (x == 2 && y == 0)
+                        )
+                        expectedImage.SetPixel(x, y, Color.Red);
+                    else if ((x == 2 && y == 2))
+                        expectedImage.SetPixel(x, y, Color.Blue);
+                    else if ((x == 4 && y == 0))
+                        expectedImage.SetPixel(x, y, Color.Green);
+                    else
+                        expectedImage.SetPixel(x, y, Color.Black);
+                }
+            #endregion
+
+
+
+            PatternCreator creator = new PatternCreator();
+            creator.Palette = new Color[] { Color.Red, Color.Green, Color.Blue };
+            creator.GridFlag = true;
+            Bitmap actual = creator.GetEmbroidery(inputImage, 3, 2);
+
+            Assert.IsTrue(actual.Height == expectedImage.Height && actual.Width == expectedImage.Width);
+
+            for (int y = 0; y < expectedImage.Height; y++)
+                for (int x = 0; x < expectedImage.Width; x++)
+                {
+                    Color actualColor = actual.GetPixel(x, y);
+                    Color expectedColor = expectedImage.GetPixel(x, y);
+                    Assert.IsTrue(actualColor == expectedColor);
+                }
+
+        }
+    }
+}

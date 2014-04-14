712f96d8b5d52e8a1a367d12e7a60ca02426b2fe
nsvova <volodymyr.nos@globallogic.com>
2014-04-14 14:04:04 +0300

    Add new method to Calculate

--- Source/NSEmbroidery.Core/Calculate.cs
+++ Source/NSEmbroidery.Core/Calculate.cs
@@ -22,12 +22,40 @@ namespace NSEmbroidery.Core
             int cellWidth = image.Width / cellsCount;
 
             if (image.Height < cellWidth)
-                throw new WrongResolutionException("Image's height must be higher");
+                throw new WrongResolutionException("Image's height must be higher or input more cells");
 
             int newHeight = image.Height / cellWidth;
             int newWidth = cellsCount;
 
             for (int i = 2; i < countResolutions + 2; i++)
+                result.Add(new Resolution(newWidth * i, newHeight * i), i);
+
+            return result;
+        }
+
+
+        public static Dictionary<Resolution, int> PossibleResolutions(Bitmap image, int cellsCount, int minCoefficient, int maxCoefficient)
+        {
+            Dictionary<Resolution, int> result = new Dictionary<Resolution, int>();
+
+            if (cellsCount <= 0)
+                throw new NotInitializedException("Square count has to be initialized and inherent");
+
+            if (image.Width < cellsCount)
+                throw new WrongResolutionException("Image's width must be higher or input less cells");
+
+            int cellWidth = image.Width / cellsCount;
+
+            if (image.Height < cellWidth)
+                throw new WrongResolutionException("Image's height must be higher or input more cells");
+
+            if (minCoefficient < 2 || minCoefficient >= maxCoefficient)
+                throw new Exception("minCoefficient has to be less than maxCoefficient and more than 2");
+
+            int newHeight = image.Height / cellWidth;
+            int newWidth = cellsCount;
+
+            for (int i = minCoefficient; i < maxCoefficient; i++)
                 result.Add(new Resolution(newWidth * i, newHeight * i), i);
 
             return result;

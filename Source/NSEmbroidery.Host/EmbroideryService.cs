dc95e4337eb144f05f79d82bcad1b7d4b7b275dd
nsvova <volodymyr.nos@globallogic.com>
2014-04-29 18:21:21 +0300

    now host opens in the same thread.

--- Source/NSEmbroidery.Host/EmbroideryService.cs
+++ Source/NSEmbroidery.Host/EmbroideryService.cs
@@ -17,48 +17,34 @@ namespace NSEmbroidery.Host
     public partial class EmbroideryService : ServiceBase
     {
         ServiceHost host = null;
-        Thread hostThread;
         EventLog log = new EventLog("EmbroideryServiceLog");
 
         public EmbroideryService()
         {
             log.Source = ("EmbroiderySource");
             InitializeComponent();
-            hostThread = new Thread(new ThreadStart(StartThread));
             
         }
 
         protected override void OnStart(string[] args)
         {
-            this.RequestAdditionalTime(0);
-            try
-            {
+
                 if (host != null)
                     host.Close();
 
-                hostThread.Start();
-            }
-            catch (Exception e)
-            {
-                log.WriteEntry("Exception in OnStart: " + e.Message);
-            }
+                try
+                {
+                    host = new ServiceHost(typeof(EmbroideryCreator));
+                    host.Open();
+                }
+                catch (Exception e)
+                {
+                    log.WriteEntry("Exception in StartThread: " + e.Message);
+                }
+
+                log.WriteEntry("Hosting has started");
         }
 
-
-        public void StartThread()
-        {
-            try
-            {
-                host = new ServiceHost(typeof(EmbroideryCreator));
-                host.Open();
-            }
-            catch (Exception e)
-            {
-                log.WriteEntry("Exception in StartThread: " + e.Message);
-            }
-
-            log.WriteEntry("Hosting has started");
-        }
 
         protected override void OnStop()
         {

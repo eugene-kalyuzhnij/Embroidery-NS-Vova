4279f1d0d1ac1d811a1b98a339d06863ab9f42c2
nsvova <volodymyr.nos@globallogic.com>
2014-05-20 19:31:14 +0300

    add new project files to repository

---
+++ Source/NSEmbroidery.Data.DI.EF/DataModelCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using Ninject.Modules;
+using NSEmbroidery.Data.Interfaces;
+using NSEmbroidery.Data.EF;
+using NSEmbroidery.Data.Models;
+
+namespace NSEmbroidery.Data.DI.EF
+{
+    public class DataModelCreator : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IRepository<User>>().To<UsersRepository>();
+            this.Bind<IRepository<Embroidery>>().To<EmbroideriesRepository>();
+        }
+    }
+}
---
+++ Source/NSEmbroidery.Data.Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSEmbroidery.Data.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        IList<T> GetAll();
+        void Add(T item);
+        void Remove(T item);
+        T GetLast();
+    }
+}
